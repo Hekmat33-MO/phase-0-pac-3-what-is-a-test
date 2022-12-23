7c18eeec7f18ae17732d67299cdf3df9ae4db070
Hekmat Mohammed <heka332020@gmail.com>
2022-12-23 14:08:57 +0300

    Done

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,6 @@
-require ( './root.js' );
+require('./root.js');
 
+const expect = require('expect/lib/index.js');
 const { name, height, message } = require("../index.js");
 
 /*
@@ -29,10 +30,26 @@ describe('what-is-a-test', () => {
 // The code below ensures that students who are using CodeGrade will get credit 
 // for the code-along in Canvas; you can disregard it.
 
-describe('', () => {
-  describe('', () => {
-    it('', () => {
-      return true;
-    })
-  })
-})
+describe('what-is-a-test', () => {
+    describe('Name', () => {
+        it('returns "Susan"', () => {
+
+            expect(name).toEqual("Susan");
+        });
+    });
+
+    describe('Height', () => {
+        it('is less than 40', () => {
+
+            expect(height).toBeLessThan(40);
+        });
+    });
+
+    describe('Message', () => {
+        it('gives the name and hieght', () => {
+
+            expect(message).toInclude(name);
+            expect(message).toInclude(height);
+        });
+    });
+});
